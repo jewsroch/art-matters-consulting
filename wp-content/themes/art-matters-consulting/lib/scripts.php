fa2816a03e679e225a4d1485367a23c18fa4eca7
Chad Jewsbury <cjewsbury@groupon.com>
2013-09-07 14:16:55 -0500

    Fixes full width image at 1200+

--- wp-content/themes/art-matters-consulting/lib/scripts.php
+++ wp-content/themes/art-matters-consulting/lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, 'a21d69317c0a0a06e1e89d335fc56dcb');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '82e0449951b415c3abe85fc51ecd771b');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
