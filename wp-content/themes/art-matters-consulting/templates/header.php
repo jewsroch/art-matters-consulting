ed85dde1d55a0808fb69d616311e163fb2e36662
Chad Jewsbury <cjewsbury@groupon.com>
2013-09-08 23:41:02 -0500

    try again - fix logo link

--- wp-content/themes/art-matters-consulting/templates/header.php
+++ wp-content/themes/art-matters-consulting/templates/header.php
@@ -2,7 +2,7 @@
     <div class="container">
         <div class="row">
             <div class="col-lg-3 logo col-md-12">
-                <a class="logo" href="/"><img src="<?php echo get_template_directory_uri() ?>/assets/img/logo.png" alt="Art Matters Consulting"/></a>
+                <a class="logo" href="<?php echo get_site_url() ?>"><img src="<?php echo get_template_directory_uri() ?>/assets/img/logo.png" alt="Art Matters Consulting"/></a>
             </div>
             <div class="col-sm-12 col-md-12 col-lg-9">
                 <nav class="nav-main" role="navigation">
