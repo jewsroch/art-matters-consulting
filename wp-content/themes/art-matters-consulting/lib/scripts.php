f04b20667acc48f0d952460b5dd67cd035c47cbb
Chad Jewsbury <cjewsbury@groupon.com>
2013-09-11 20:42:05 -0500

    Fixes buttons and lists

--- wp-content/themes/art-matters-consulting/lib/scripts.php
+++ wp-content/themes/art-matters-consulting/lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '72833b8e1e6102756897f79e719030bd');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '4786e2ed06fa0b42dc0fe554dfd6efdf');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
