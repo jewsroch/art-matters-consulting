f72ae121ee306752da0281635b190c5a02ecab4f
Chad Jewsbury <cjewsbury@groupon.com>
2013-09-14 16:25:15 -0500

    Adds respond.js for IE

--- wp-content/themes/art-matters-consulting/templates/head.php
+++ wp-content/themes/art-matters-consulting/templates/head.php
@@ -9,6 +9,9 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
   <?php wp_head(); ?>
+    <!--[if lte IE 8]>
+    <script type='text/javascript' src='<?php echo get_template_directory_uri(); ?>/assets/js/vendor/respond.min.js'></script>
+    <![endif]-->
 
   <link href='http://fonts.googleapis.com/css?family=Lato:300,400,700,300italic,400italic|Merriweather:400,300,300italic,400italic,700' rel='stylesheet' type='text/css'>
   <link rel="alternate" type="application/rss+xml" title="<?php echo get_bloginfo('name'); ?> Feed" href="<?php echo home_url(); ?>/feed/">
