6a76d3e856c6b79553d594f5faa25c72fdbbce63
Chad Jewsbury <cjewsbury@groupon.com>
2013-09-15 12:37:53 -0500

    Adds dynamic menu to footer

--- wp-content/themes/art-matters-consulting/lib/scripts.php
+++ wp-content/themes/art-matters-consulting/lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, 'dc857646d35cd7bd53661dd2dfed814f');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '58e3edd0782b048803c713080526073d');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
--- wp-content/themes/art-matters-consulting/templates/footer.php
+++ wp-content/themes/art-matters-consulting/templates/footer.php
@@ -2,7 +2,30 @@
     <div class="container">
         <div class="row">
             <div class="col-sm-8 col-xs-12 footer-nav">
-                <div class="row">
+                <?php
+                $defaults = array(
+                    'theme_location'  => 'footer_navigation',
+                    'menu'            => '',
+                    'container'       => 'div',
+                    'container_class' => 'row',
+                    'container_id'    => '',
+                    'menu_class'      => 'list-unstyled',
+                    'menu_id'         => '',
+                    'echo'            => true,
+                    'fallback_cb'     => 'wp_page_menu',
+                    'before'          => '',
+                    'after'           => '',
+                    'link_before'     => '',
+                    'link_after'      => '',
+                    'items_wrap'      => '<ul id="%1$s" class="%2$s">%3$s</ul>',
+                    'depth'           => 0,
+                    'walker'          => ''
+                );
+
+                wp_nav_menu( $defaults );
+
+                ?>
+                <!--<div class="row">
                     <ul class="list-unstyled">
                         <div class="col-sm-4">
                             <li class="active"><a href="#">Home</a></li>
@@ -16,7 +39,7 @@
                             <li><a href="#">Contact Us</a></li>
                         </div>
                     </ul>
-                </div>
+                </div>-->
             </div>
             <div class="col-xs-12 col-sm-4 footer-icon">
                 <a href="">
