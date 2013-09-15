aebd5b67fa3182190510f391cbbba23048db18d2
Chad Jewsbury <cjewsbury@groupon.com>
2013-09-15 14:38:54 -0500

    Cleans up styles for other widgets"

--- wp-content/themes/art-matters-consulting/lib/scripts.php
+++ wp-content/themes/art-matters-consulting/lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '518b817542d0b3cc3502510d3947a764');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '53b2c10d26331649397a8743b18a32f3');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
