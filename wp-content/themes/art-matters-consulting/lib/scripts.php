14f23b13511fee497d1cedaa55e8fe911325e932
Chad Jewsbury <cjewsbury@groupon.com>
2013-09-15 20:05:06 -0500

    Cleans up form styles

--- wp-content/themes/art-matters-consulting/lib/scripts.php
+++ wp-content/themes/art-matters-consulting/lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '9e7de9d57ef272f0bb39b3a0920db5ca');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, 'a6a38bdc93a179ee2093df56d70ece52');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
