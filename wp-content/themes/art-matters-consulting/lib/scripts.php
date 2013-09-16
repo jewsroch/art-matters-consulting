45a9a5b43cc03a08f13c4aa207e5b36f1197e34e
Chad Jewsbury <cjewsbury@groupon.com>
2013-09-15 19:55:13 -0500

    Patches bootstrap styles into main for form

--- wp-content/themes/art-matters-consulting/lib/scripts.php
+++ wp-content/themes/art-matters-consulting/lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '6cdec04bb595e35cf386adc8b8f1a863');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '4d08e8927976dfe3089e428980006859');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
