4cee6cec4fd48ff57d5bff0d2e74c343d81dd0fa
Chad Jewsbury <cjewsbury@groupon.com>
2013-09-15 20:43:00 -0500

    Adds style guide admin link in footer

--- wp-content/themes/art-matters-consulting/templates/footer.php
+++ wp-content/themes/art-matters-consulting/templates/footer.php
@@ -24,22 +24,12 @@
 
                 wp_nav_menu( $defaults );
 
+                $styleGuide = get_page_by_title('Style Guide');
+                if (is_admin() && isset($styleGuide) ) {
+                    echo '<a href="' . get_permalink($styleGuide->ID) . '">Style Guide</a>';
+                }
                 ?>
-                <!--<div class="row">
-                    <ul class="list-unstyled">
-                        <div class="col-sm-4">
-                            <li class="active"><a href="#">Home</a></li>
-                            <li><a href="#">About Us</a></li>
-                            <li><a href="#">For Collectors</a></li>
-                            <li><a href="#">For Institutions</a></li>
-                        </div>
-                        <div class="col-sm-4">
-                            <li><a href="#">Publications</a></li>
-                            <li><a href="#">Representative Projects</a></li>
-                            <li><a href="#">Contact Us</a></li>
-                        </div>
-                    </ul>
-                </div>-->
+
             </div>
             <div class="col-xs-12 col-sm-4 footer-icon">
                 <a href="">
