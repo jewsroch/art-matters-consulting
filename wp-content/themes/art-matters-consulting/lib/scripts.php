919a95d39df4416a7606783fd16c2b529800de44
Chad Jewsbury <cjewsbury@groupon.com>
2013-09-15 20:11:12 -0500

    hides h3 again

--- wp-content/themes/art-matters-consulting/lib/scripts.php
+++ wp-content/themes/art-matters-consulting/lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, 'b0fb15b94539981a1ffbe1114b744055');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '9e977ab55d498fc5e56c9c2e9197294c');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
